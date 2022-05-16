99ace3a49faf0cbd382afe6a2055b181270f0858
OnyxFire1 <alexneil179@gmail.com>
2022-05-16 16:01:57 -0700

    completed week 1 assessment

--- code-challenges.js
+++ code-challenges.js
@@ -7,43 +7,102 @@
 // --------------------INSTRUCTOR EXAMPLE: Create a function that takes in any two strings and returns the string with more characters. Use the two sets of test variables provided.
 
 // Set one:
-const fruit1 = "apple"
-const fruit2 = "banana"
+// const fruit1 = "apple"
+// const fruit2 = "banana"
 
-// Set two:
-const fruit3 = "cherry"
-const fruit4 = "kiwi"
+// // Set two:
+// const fruit3 = "cherry"
+// const fruit4 = "kiwi"
 
 
 // --------------------1) Create a function that takes in any given number and determines if the number is below boiling point, at boiling point, or above boiling point. Boiling point is 212 degrees Fahrenheit. Use the test variables provided below. Expected output: "42 is below boiling point", "350 is above boiling point", "212 is at boiling point"
 
-const temp1 = 42
-const temp2 = 350
-const temp3 = 212
+// The goal is to create a function that takes the numbers listed and determine whether they are at, below, or above boiling point
 
+// If temp is > 212 return below boiling point
+
+// If temp is < 212 return above boiling point
+
+// If temp is === 212 return at boiling point
+
+
+// const temp1 = 42
+// const temp2 = 350
+// const temp3 = 212
+
+
+        // const boilingPoint = (number) => {
+        // if(number < 212) {
+        //     return `${number} is below boiling point`
+        // } else if (number > 212) {
+        //     return `${number} is above boiling point`
+        // } else if (number === 212) {
+        //     return `${number} is at boiling point`
+    //     }
+    // }
+        // console.log(boilingPoint(350))
+        // console.log(boilingPoint(42))
+        // console.log(boilingPoint(212))
+
+
+// We need to create a code that takes the two arrays and return length 
+
+// .concat will merge both myNumbers1 and myNumbers2
+
+//  comboArrays all the combination length elements will show in the array
 
 // --------------------2) Create the code that will combine the two arrays and return the length using the variables provided below. Expected output: 10
 
-const myNumbers1 = [3, 7, 0, 36, -9]
-const myNumbers2 = [8, -7, 42, 9, 13]
+// const myNumbers1 = [3, 7, 0, 36, -9]
+// const myNumbers2 = [8, -7, 42, 9, 13]
 
 
-// --------------------3) Create the code that will reverse the letters of a string using the test variable provided below. Expected output: "2202 eilrahC"
+//     console.log(myNumbers1.length + myNumbers2.length)
 
-const currentCohort = "Charlie 2022"
+// To get the results of "2022 eilrahC" it was essential to Loop through the Array one end to the other and push everything in the Array. Then use .reverse and .join in the string
 
+//  --------------------3) Create the code that will reverse the letters of a string using the test variable provided below. Expected output: "2022 eilrahC"
+
+
+// const currentCohort = "Charlie 2022"
+// let string = "Charlie 2022"
+// string = [...string].reverse().join ("")
+
+// Since this is a conditional statement the iteration of index shows the position of an expression match. By using the for loop will let us know where the count index starts, how many iterations we want going through the loop, and what is to be done after each loop.
 
 // --------------------4) Create a statement that will evaluate whether each number in an array is even or odd. Use the test variable provided below. Expected output: "odd" "even" "odd" "even" "odd" "even"
 
-const myArray = [13, 34, 5, 10, 27, 42]
+//  const myArray = [13, 34, 5, 10, 27, 42]
 
+//  for(let i=0; i < myArray.length; i==) {
+//      if (myArray[i]%2 === 0) 
+//          console.log("even")
+//      } else {}
+//         console.log("odd")
+
+
+//  In this example the was to take a set of numbers and subtract the smaller from the larget and return an postitive result. I'm not sure how to break this down but when I verified it came back with the required answer.
 
 // --------------------5) Create a function that takes in a set of any two numbers and subtracts the smaller number from the larger number using the sets of test variables provided below. Expected output: 42, 3
 
 // Set one:
-const number1 = 58
-const number2 = 100
+// const number1 = 58
+// const number2 = 100
 
-// Set two:
-const number3 = 27
-const number4 = 24
+// const getDifference = (num1, num2) => {
+    
+//     return num2 - num1
+// }
+// console.log (getDifference (58, 100))
+
+// function
+
+//  Set two:
+// const number3 = 27
+// const number4 = 24
+
+// const getDifference = (num3, num4) => {
+    
+//     return num3 - num4
+// }
+// console.log (getDifference (27, 24))
--- coding-concepts.js
+++ coding-concepts.js
@@ -6,54 +6,59 @@
 // Then, uncomment the console.log() and verify the output. Briefly explain why your initial answer was correct or incorrect.
 
 // --------------------INSTRUCTOR EXAMPLE: What will this log?
-const colors = ["tangerine", "magenta", "lilac", "daffodil"]
-// console.log(colors.push("indigo"))
+// const colors = ["tangerine", "magenta", "lilac", "daffodil"]
+// // console.log(colors.push("indigo"))
 
-// a) Your answer:
-// b) Verify and explain:
+// // a) Your answer:
+// // b) Verify and explain:
 
 
 // --------------------1) What will this log?
 
-const cohort = "LEARN 2022"
+// const cohort = "LEARN 2022"
 // console.log(cohort.length)
 
-// a) Your answer:
-// b) Verify and explain:
+
+// a) Your answer: The answer to Learn 2022 should be 10 becuase the string is five character, the space which is data and 2022
+// b) Verify and explain: After running the code the answer was 10
 
 
 // --------------------2) What will this log?
 
-const greeting = "Hello World!"
+// const greeting = "Hello World!"
 // console.log(greeting[4])
 
-// a) Your answer:
-// b) Verify and explain:
+
+// a) Your answer: The answer should be "o" in Hello because the index is 4
+// b) Verify and explain: The index gives the position of the first occurrency and that first occurence would be "o"
 
 
 // --------------------3) What will this log?
 
-const languages = ["JavaScript", "Ruby", "Python", "C++"]
-const index = 1
+// const languages = ["JavaScript", "Ruby", "Python", "C++"]
+// const index = 1
 // console.log(languages[index])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: Since index is 1 the answer should be Ruby
+// b) Verify and explain: The correct answer is Ruby based on the same index explanation in question 2
 
 
 // --------------------4) What will this log?
 
-const weekendDays = ["saturday", "sunday"]
+// const weekendDays = ("saturday", "sunday")
 // console.log(weekendDays.toUpperCase())
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: I expected to get Saturday in lower case and Sunday in caps.
+// b) Verify and explain: I removed the square bracketts and repalced it with parenthesis but only Sunday in caps returned. Probably had to type out the command instead of hitting the up arrow.
 
 
 // --------------------5) What will this log?
 
-const dataTypes = ["number", "string", "Boolean"]
+// const dataTypes = ["number", "string", "Boolean"]
 // console.log(typeof dataTypes.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: The answer should be number because .length deals with number
+// b) Verify and explain: I ran the code in terminal as is and the right answer was number, however I removed typeof and the answer came back 3
+
+
+
